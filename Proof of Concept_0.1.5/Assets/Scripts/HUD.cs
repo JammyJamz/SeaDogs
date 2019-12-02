a72f84917cf5795350c2323b6441530f12865e13
JammyJamz <33839743+JammyJamz@users.noreply.github.com>
2019-12-02 11:38:23 -0500

    Add files via upload
    HUD = character image swapping
    HealthBarController = health bar going up or down

---
+++ Proof of Concept_0.1.5/Assets/Scripts/HUD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+
+    public Sprite saltyImg;
+    public Sprite rustyImg;
+    private bool change = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Image>().sprite = saltyImg;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T) && gameObject.GetComponent<Image>().sprite == saltyImg)
+        {
+            gameObject.GetComponent<Image>().sprite = rustyImg;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.T) && gameObject.GetComponent<Image>().sprite == rustyImg)
+        {
+            gameObject.GetComponent<Image>().sprite = saltyImg;
+        }
+            
+
+    }
+}
---
+++ Proof of Concept_0.1.5/Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    private Slider healthbar;
+    private int currentHealth = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthbar = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthbar.value = currentHealth;
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            currentHealth = currentHealth - 1; ;
+        }
+    }
+}
